8adf89cfa1d796dedd8c272117bec5a122a6a8a1
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-08 11:52:20 -0500

    main js function works and submit new book mark fired

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
 'use strict';
 
+/*eslint-env jquery*/
+/* global Api, Store, Bookmarks*/
+
+$(main);
+
+function main() {
+  $('.js-bookmark-add-form').on('submit', e => {
+    e.preventDefault();
+    console.log('submitted js-bookmark-add-form');
+  });
+}
 
