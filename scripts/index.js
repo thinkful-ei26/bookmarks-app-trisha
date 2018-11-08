78f251789fd538fdcbf6c7941b5486a57f12af11
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-08 12:07:33 -0500

    index.js returning an JSON.stringify obj with bookmark items. Next step: create store.js to STORE the bookmark list. Then render it to the results list on DOM

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,26 @@
 /*eslint-env jquery*/
 /* global Api, Store, Bookmarks*/
 
-$(main);
+$.fn.extend({
+  serializeJson: function() {
+    const obj = {};
+    const data = new FormData(this[0]);
+    data.forEach((value, key) => {
+      obj[key] = value;
+    });
+    return JSON.stringify(obj);
+  }
+});
 
 function main() {
   $('.js-bookmark-add-form').on('submit', e => {
     e.preventDefault();
-    console.log('submitted js-bookmark-add-form');
+    const result = $(e.target).serializeJson(); 
+    
+    console.log('this is e.target',e.target);
+
+    console.log('this is result of .serializeJson', result);
   });
 }
 
+$(main);
